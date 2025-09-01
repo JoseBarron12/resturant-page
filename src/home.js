de79ad3c6dd81cb885cffe7ab0dccacb884a5411
JoseBarron12 <143546100+JoseBarron12@users.noreply.github.com>
2025-09-01 18:40:17 -0500

    Add displayHome() to display home page using js.
    Div#content is now empty and page is entirely built
    using js, to allow navbar button functionality to
    work for future page sections. On page load this
    page will automatically load.

---
+++ src/home.js
@@ -0,0 +1,35 @@
+const displayHome = () => {
+    const content = document.querySelector("#content");
+    
+    const background = document.createElement("div");
+    background.classList.add("main-background");
+    content.appendChild(background);
+    
+    const text = document.createElement("div");
+    text.classList.add("main-text");
+    background.appendChild(text);
+
+    const header = document.createElement("h1");
+    header.textContent = "Random Resturant Name";
+
+    const paragraph = document.createElement("p");
+    paragraph.textContent = "This is one of the best, if not best resturants ever.\r\nRated very highly by all.";
+    
+    const buttons = document.createElement("div");
+    buttons.classList.add("main-buttons");
+    
+    text.appendChild(header);
+    text.appendChild(paragraph);
+    text.appendChild(buttons);
+
+    const reviewBtn = document.createElement("button");
+    reviewBtn.textContent = "Reviews";
+    
+    const learnBtn = document.createElement("button");
+    learnBtn.textContent = "Learn More";
+
+    buttons.appendChild(reviewBtn);
+    buttons.appendChild(learnBtn);
+};
+
+export {displayHome};
--- src/index.js
+++ src/index.js
@@ -1,1 +1,4 @@
 import "./styles.css";
+import { displayHome } from "./home";
+
+displayHome();
