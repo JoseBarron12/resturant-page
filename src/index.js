970fb44265c7b3c1602d92e0421431240618826d
JoseBarron12 <143546100+JoseBarron12@users.noreply.github.com>
2025-09-02 20:19:17 -0500

    Add styling to navbar button of current page.
    User able to easily distinguish which page they are
    currently on to avoid confusion.

--- src/index.js
+++ src/index.js
@@ -3,7 +3,10 @@ import { displayHomePage } from "./home";
 import { displayMenuPage } from "./menu";
 import { displayAboutPage } from "./about";
 
+
 displayHomePage();
+const headerText = document.querySelector("nav>button.home-button>h4");
+headerText.classList.add("active");
 
 const navButtons = document.querySelectorAll("nav>button");
 navButtons.forEach((button)=> {
@@ -11,18 +14,29 @@ navButtons.forEach((button)=> {
         const content = document.querySelector("#content");
         content.replaceChildren();
 
+        const headerTexts = document.querySelectorAll("nav>button>h4");
+        headerTexts.forEach((navbarText) => {
+            navbarText.classList.remove("active");
+        });
+
         const buttonName = button.getAttribute("class");
         
         if(buttonName == "home-button")
         {
+            const headerText = document.querySelector("nav>button.home-button>h4");
+            headerText.classList.add("active");
             displayHomePage();
         }
         else if (buttonName == "menu-button")
         {
+            const headerText = document.querySelector("nav>button.menu-button>h4");
+            headerText.classList.add("active");
             displayMenuPage();
         }
         else
         {
+            const headerText = document.querySelector("nav>button.about-button>h4");
+            headerText.classList.add("active");
             displayAboutPage();
         }
     });
